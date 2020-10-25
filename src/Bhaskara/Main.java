a79e3119a9fe9ffca24394676bb5b9cafda8b2c3
Gabriel Bugliani Silva <gbuglian@everis.com>
2020-10-25 18:04:40 -0300

    Bhaskara em Programação Orientada a Objeto

---
+++ src/Bhaskara/Main.java
@@ -0,0 +1,32 @@
+package Bhaskara;
+
+public class Main {
+	public static void main(String[] args) {
+		Bhaskara bhaskara = new Bhaskara();
+
+		do {
+			bhaskara.ObtendoValor_A();
+
+			if (bhaskara.A <= 0) {
+				System.out.println("Erro: Insira um valor maior que 0");
+			}
+
+		} while (bhaskara.A <= 0);
+
+		bhaskara.ObtendoValor_B();
+
+		bhaskara.ObtendoValor_C();
+
+		bhaskara.ObterDelt();
+
+		bhaskara.ObterRaiz();
+
+		bhaskara.CalculoX();
+
+		bhaskara.CalculoX2();
+
+		System.out.println("Valor da raiz de delta é:" + bhaskara.RaizDelta);
+		System.out.println("Valor da equação x1:" + bhaskara.ValorX);
+		System.out.println("Valor da equação x2:" + bhaskara.ValorX2);
+	}
+}
